a6fd9b80f6e08dffb6165d85cb5ffe2c9e36a537
Josh Lovering <jlovering@wpi.edu>
2024-02-19 14:45:46 -0500

    uniform berry distribution, added sensor class

--- classes.py
+++ classes.py
@@ -10,6 +10,7 @@ from pygame.locals import (
 )
 import random
 from var import *
+import math
 
 # Player -> Creature
 # Prey -> Animal -> Creature
@@ -24,13 +25,8 @@ class Creature(pygame.sprite.Sprite):
         # create sprite
         super(Creature, self).__init__()
 
-class Animal(Creature):
-    def __init__(self, color, size, speed, sense):
-        super(Animal, self).__init__(color, size, speed)
-        self.sense = sense
-
 class Player(Creature):
-    def __init__(self, color, size, speed):
+    def __init__(self, color=COLOR_PLAYER, size=20, speed=2):
         super(Player, self).__init__(color, size, speed)
         self.surf = pygame.Surface((self.size, self.size*1.5))
         self.surf.fill(self.color)
@@ -55,8 +51,16 @@ class Player(Creature):
         if self.rect.bottom >= SCREEN_HEIGHT-BUFFER/2:
             self.rect.bottom = SCREEN_HEIGHT-BUFFER/2
 
-class Prey(Animal):
+class Animal(Creature):
     def __init__(self, color, size, speed, sense):
+        super(Animal, self).__init__(color, size, speed)
+        self.sense = sense
+    def create_sensor(self):
+        add_sensor_event = pygame.event.Event(ADDSENSOR, animal=self)
+        pygame.event.post(add_sensor_event)
+
+class Prey(Animal):
+    def __init__(self, color=COLOR_PREY, size=20, speed=1, sense=100):
         super(Prey, self).__init__(color, size, speed, sense)
         self.surf = pygame.Surface((self.size, self.size))
         self.surf.fill(self.color)
@@ -66,22 +70,29 @@ class Prey(Animal):
                 random.randint(0+BUFFER, SCREEN_HEIGHT-BUFFER),
             )
         )
+        self.create_sensor()
         self.prev_move_x = 0
         self.prev_move_y = 0
-    def update(self):
+    def move_random(self):
         # apply movement based on prior frame
         if self.prev_move_x == -1:
-            move_x = random.choices([-1,0,1],[0.9,0.09,0.01])[0]
+            move_x = random.choices([-1,0,1],[0.95,0.04,0.01])[0]
         if self.prev_move_x == 0:
             move_x = random.randint(-1, 1)
         if self.prev_move_x == 1:
-            move_x = random.choices([-1,0,1],[0.01,0.09,0.9])[0]
+            move_x = random.choices([-1,0,1],[0.01,0.04,0.95])[0]
         if self.prev_move_y == -1:
-            move_y = random.choices([-1,0,1],[0.9,0.09,0.01])[0]
+            move_y = random.choices([-1,0,1],[0.95,0.04,0.01])[0]
         if self.prev_move_y == 0:
             move_y = random.randint(-1, 1)
         if self.prev_move_y == 1:
-            move_y = random.choices([-1,0,1],[0.01,0.09,0.9])[0]
+            move_y = random.choices([-1,0,1],[0.01,0.04,0.95])[0]
+        return move_x, move_y
+    # def move_react(self):
+        # reaction order, Predator > Player > Needs
+        # needs = food or mate
+    def update(self):
+        move_x, move_y = self.move_random()
         # move_x = random.randint(-1, 1)
         # move_y = random.randint(-1, 1)
         self.rect.move_ip(move_x*self.speed, move_y*self.speed)
@@ -89,8 +100,24 @@ class Prey(Animal):
         self.prev_move_x = move_x
         self.prev_move_y = move_y
 
+class Sensor(pygame.sprite.Sprite):
+    def __init__(self, animal, color=COLOR_SENSOR, size=100):
+        self.animal = animal
+        self.color = color
+        self.size = size
+        # create sprite
+        super(Sensor, self).__init__()
+        # draw
+        self.draw()
+        self.rect = self.surf.get_rect(center=(self.animal.rect.x, self.animal.rect.y))
+    def draw(self):
+        self.surf = pygame.Surface((self.size*2,self.size*2),pygame.SRCALPHA)
+        pygame.draw.circle(self.surf, self.color, (self.size, self.size), self.size, self.size)
+    def update(self):
+        self.rect.center = self.animal.rect.center
+
 class Plant(pygame.sprite.Sprite):
-    def __init__(self, color, size, age, growth, berries):
+    def __init__(self, color=COLOR_PLANT, size=4, age=1, growth=1, berries=0):
         self.color = color
         self.size = size
         self.age = age
@@ -128,21 +155,26 @@ class Plant(pygame.sprite.Sprite):
             self.next_growth += PLANT_GROWTH_INTERVAL
 
 class Berry(pygame.sprite.Sprite):
-    def __init__(self, color, size, plant):
+    def __init__(self, plant, color=COLOR_BERRY, size=1):
+        self.plant = plant
         self.color = color
         self.size = size
-        self.plant = plant
         # create sprite
         super(Berry, self).__init__()
         # draw
         self.draw()
-        print(plant.rect.x,plant.rect.x+size*2)
-        self.rect = self.surf.get_rect(
-            center=(
-                random.randint(plant.rect.x, plant.rect.x+plant.size*2),
-                random.randint(plant.rect.y, plant.rect.y+plant.size*2),
-            )
-        )
+        self.place_berry()
+    def place_berry(self):
+        # Thanks to aioobe's equation here
+        # https://stackoverflow.com/questions/5837572/generate-a-random-point-within-a-circle-uniformly
+        radius = self.plant.size
+        centerX = self.plant.rect.x + radius
+        centerY = self.plant.rect.y + radius
+        r = radius * math.sqrt(random.uniform(0,1))
+        theta = random.uniform(0,1) * 2 * math.pi
+        x = int(centerX + r * math.cos(theta))
+        y = int(centerY + r * math.sin(theta))
+        self.rect = self.surf.get_rect(center=(x,y))
     def draw(self):
         self.surf = pygame.Surface((self.size*2,self.size*2),pygame.SRCALPHA)
         pygame.draw.circle(self.surf, self.color, (self.size, self.size), self.size, self.size)
--- game.py
+++ game.py
@@ -11,6 +11,7 @@ from pygame.locals import (
 import random
 from classes import *
 from var import *
+import math
 
 # initialize pygame
 pygame.init()
@@ -33,21 +34,25 @@ pygame.time.set_timer(ADDPLANT, int(6000/PLAY_SPEED_MOD))
 
 # sprite groups
 prey_group = pygame.sprite.Group()
+sensor_group = pygame.sprite.Group()
 plant_group = pygame.sprite.Group()
 berry_group = pygame.sprite.Group()
 all_group = pygame.sprite.Group()
 
 # player object
-player = Player(COLOR_PLAYER,20,2)
+player = Player()
 all_group.add(player)
 
 # initial prey object
-new_prey = Prey(COLOR_PREY,20,1,1)
+new_prey = Prey()
 prey_group.add(new_prey)
 all_group.add(new_prey)
+new_sensor = Sensor(new_prey)
+sensor_group.add(new_sensor)
+all_group.add(new_sensor)
 
 # initial plant object
-new_plant = Plant(COLOR_PLANT,4,1,1,0)
+new_plant = Plant()
 plant_group.add(new_plant)
 all_group.add(new_plant)
 
@@ -61,36 +66,63 @@ while running:
         # QUIT - window close      
         if event.type == pygame.QUIT: 
             running = False
-        # add new prey
-        elif event.type == ADDPREY:
-            new_prey = Prey(COLOR_PREY,20,1,1)
-            prey_group.add(new_prey)
-            all_group.add(new_prey)
+        # # add new prey
+        # elif event.type == ADDPREY:
+        #     new_prey = Prey(
+        #         color=COLOR_PREY,
+        #         size=20,
+        #         speed=1,
+        #         sense=100
+        #         )
+        #     prey_group.add(new_prey)
+        #     all_group.add(new_prey)
+        # # add new sensor
+        # elif event.type == ADDSENSOR:
+        #     new_sensor= Sensor(
+        #         animal=event.animal,
+        #         color=COLOR_SENSOR,
+        #         size=100,
+        #         )
+        #     sensor_group.add(new_sensor)
+        #     all_group.add(new_sensor)
         # add new plant
-        # elif event.type == ADDPLANT:
-        #     new_plant = Plant(COLOR_PLANT,4,1,1,0)
-        #     plant_group.add(new_plant)
-        #     all_group.add(new_plant)
+        elif event.type == ADDPLANT:
+            new_plant = Plant(
+                color=COLOR_PLANT,
+                size=4,
+                age=1,
+                growth=1,
+                berries=0
+                )
+            plant_group.add(new_plant)
+            all_group.add(new_plant)
         # add new berry
         elif event.type == ADDBERRY:
-            new_berry = Berry(COLOR_BERRY,1,event.plant)
+            new_berry = Berry(
+                plant=event.plant,
+                color=COLOR_BERRY,
+                size=1,
+                )
             berry_group.add(new_berry)
             all_group.add(new_berry)
 
     # user input
     pressed_keys = pygame.key.get_pressed()
 
-    # update player location
-    player.update(pressed_keys)
-
-    # update prey locations
-    prey_group.update()
-
     # update plant locations
     plant_group.update()
 
     # update berry locations
     berry_group.update()
+
+    # update prey locations
+    sensor_group.update()
+
+    # update prey locations
+    prey_group.update()
+
+    # update player location
+    player.update(pressed_keys)
 
     # reset screen and surface
     screen.fill(COLOR_BG) 
@@ -104,8 +136,17 @@ while running:
     )
 
     # draw sprites
-    for entity in all_group:
+    # for entity in all_group:
+    #     screen.blit(entity.surf, entity.rect)
+    for entity in plant_group:
         screen.blit(entity.surf, entity.rect)
+    for entity in berry_group:
+        screen.blit(entity.surf, entity.rect)
+    for entity in sensor_group:
+        screen.blit(entity.surf, entity.rect)
+    for entity in prey_group:
+        screen.blit(entity.surf, entity.rect)
+    screen.blit(player.surf, player.rect)
 
     # update display
     pygame.display.flip()
